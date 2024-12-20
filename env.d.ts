a1b68f29fd79f25564e53586dbd77828ba8e8d15
Paloma Piot <paloma.piot@udc.es>
2024-12-20 17:15:27 +0100

    env config

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
+interface ImportMetaEnv {
+    readonly VITE_BACKEND_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
